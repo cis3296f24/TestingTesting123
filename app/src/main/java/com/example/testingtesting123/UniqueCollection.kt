6532282d0644236453077a7bb6d5030002fad105
Youngdoul <157045613+Youngdoul@users.noreply.github.com>
2024-12-04 15:33:14 -0500

    Update UniqueCollection.kt

--- app/src/main/java/com/example/testingtesting123/UniqueCollection.kt
+++ app/src/main/java/com/example/testingtesting123/UniqueCollection.kt
@@ -3,32 +3,21 @@ package com.example.testingtesting123
 class UniqueCollection {
     private val collection = ArrayList<Item>()
 
-
-    /** Add an item to the collection */
-    fun addItem (item: Item) {
+    fun addItem(item: Item) {
         if (!collection.contains(item))
             collection.add(item)
     }
 
-    /** Return an element from the collection at the specified position */
-    fun get (position: Int) = collection[position]
+    fun get(position: Int) = collection[position]
 
 
-    /** Remove an item from the collection */
-    fun remove (item: Item) {
+    fun remove(item: Item) {
         !collection.remove(item)
     }
 
-
-    // TODO 3: Complete this function such that the pre-written Unit Test (TDD) passes
-
-    /** Clear all items from hte collection */
-    fun clear () {
+    fun clear() {
 
     }
 
-    /** Return the side of the collection */
     fun size() = collection.size
-
-
 }
